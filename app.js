a6e411ccbefd5e0710f4d48ed05196f20cd032bf
Dinija Seferović <64585658+DinijaSeferovic@users.noreply.github.com>
2020-08-19 23:55:04 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+const bars=document.querySelectorAll('.progress-bar');
+
+bars.forEach(function(bar){
+    let percentage= bar.dataset.percent;
+    let tooltip= bar.children[0];
+    tooltip.innerText=percentage+'%';
+    bar.style.width= percentage+'%';
+    console.log(percentage);
+})
+
+
+
+
+
+
+});
