d68e3c75253cb9181227827ff1fb980d673ea615
Dinija Seferović <64585658+DinijaSeferovic@users.noreply.github.com>
2020-08-31 04:10:41 +0200

    Create app.js

---
+++ javascript/app.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+const bars=document.querySelectorAll('.progress-bar');
+
+bars.forEach(function(bar){
+    let percentage= bar.dataset.percent;
+    let tooltip= bar.children[0];
+    tooltip.innerText=percentage+'%';
+    bar.style.width= percentage+'%';
+    console.log(percentage);
+})
+
+window.addEventListener("load", function () {
+      setTimeout(function(){
+      const loader = document.querySelector(".loader");
+      loader.className += " hidden"; // class "loader hidden"
+      const loader_bg = document.querySelector(".loader_bg");
+      loader_bg.className += " hidden"; // class "loader_bg hidden"
+      }, 500);
+
+})
+
+});
+
+
