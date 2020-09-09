f447380d83658c129c6d63a08ecbd1c56717b87e
Dinija Seferović <64585658+DinijaSeferovic@users.noreply.github.com>
2020-09-09 04:05:21 +0200

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -16,9 +16,24 @@ window.addEventListener("load", function () {
       loader.className += " hidden"; // class "loader hidden"
       const loader_bg = document.querySelector(".loader_bg");
       loader_bg.className += " hidden"; // class "loader_bg hidden"
-      }, 700);
+      }, 500);
 
-})
+});
+
+const menu = document.querySelector(".menu");
+const times = document.querySelector(".times");
+const mobileNav = document.querySelector(".mobile-nav");
+
+menu.addEventListener("click", function () {
+    mobileNav.classList.add("open");
+});
+
+times.addEventListener("click", function () {
+    mobileNav.classList.remove("open");
+});
+
+menu.removeEventListener("click");
+times.removeEventListener("click");
 
 });
 
