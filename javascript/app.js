7bbbdbaac52824aa8635ce5966634e1736cddf2c
Dinija Seferović <64585658+DinijaSeferovic@users.noreply.github.com>
2022-02-27 17:36:08 +0100

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -4,19 +4,14 @@ const bars=document.querySelectorAll('.progress-bar');
 
 bars.forEach(function(bar){
     let percentage= bar.dataset.percent;
-    let tooltip= bar.children[0];
-    tooltip.innerText=percentage+'%';
     bar.style.width= percentage+'%';
-    console.log(percentage);
 })
 
 window.addEventListener("load", function () {
-      setTimeout(function(){
-      const loader = document.querySelector(".loader");
-      loader.className += " hidden"; // class "loader hidden"
-      const loader_bg = document.querySelector(".loader_bg");
-      loader_bg.className += " hidden"; // class "loader_bg hidden"
-      }, 500);
+    const loader = document.querySelector(".loader");
+    loader.className += " hidden"; // class "loader hidden"
+    const loader_bg = document.querySelector(".loader_bg");
+    loader_bg.className += " hidden"; // class "loader_bg hidden"
 
 });
 
